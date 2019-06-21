9833e0c90b8305933a99e471d4dde5144c6d44d4
gpuCI <rapids@ai>
2019-06-21 09:05:39 +0000

    AUTO gpuCI Docs Update

--- libnvstrings/0.8.0/search/functions_2.js
+++ libnvstrings/0.8.0/search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['capitalize',['capitalize',['../classNVStrings.html#a2c71da05b27c0df1488a55ca5985f266',1,'NVStrings']]],
-  ['cat',['cat',['../classNVStrings.html#a6bd101e83b76d12667d4061e34e1833e',1,'NVStrings']]],
+  ['cat',['cat',['../classNVStrings.html#a6bd101e83b76d12667d4061e34e1833e',1,'NVStrings::cat(NVStrings *others, const char *separator, const char *narep=0)'],['../classNVStrings.html#ac19bf8db0608802e521cef3358e378aa',1,'NVStrings::cat(std::vector&lt; NVStrings *&gt; &amp;others, const char *separator, const char *narep=0)']]],
   ['center',['center',['../classNVStrings.html#ac896e5ce494a3d901e598822a7ed61d9',1,'NVStrings']]],
   ['compare',['compare',['../classNVStrings.html#a97f105ecf0e8ecc2f9b1a2b8f2df99e6',1,'NVStrings']]],
   ['compute_5fstatistics',['compute_statistics',['../classNVStrings.html#add6efee15f3cbc7cf2f433c60326e86e',1,'NVStrings']]],
@@ -17,7 +17,7 @@ var searchData=
   ['create_5ffrom_5fcsv',['create_from_csv',['../classNVStrings.html#a91c022a172bc0988862d1ebc09e099e4',1,'NVStrings']]],
   ['create_5ffrom_5findex',['create_from_index',['../classNVStrings.html#a8cadd917f9e5a1336eb1ba7416abc988',1,'NVStrings::create_from_index()'],['../classNVCategory.html#a6ecb7cd1bf614bbcd6007d8d0cf943c8',1,'NVCategory::create_from_index()']]],
   ['create_5ffrom_5fipc',['create_from_ipc',['../classNVStrings.html#a33c3292e04f327176eaa9cfd961e673f',1,'NVStrings::create_from_ipc()'],['../classNVCategory.html#a55e3c429cfff7f7e679f6b8514125438',1,'NVCategory::create_from_ipc()']]],
-  ['create_5ffrom_5foffsets',['create_from_offsets',['../classNVStrings.html#a69e9e93de1a21474f9732bad28fdd035',1,'NVStrings::create_from_offsets()'],['../classNVCategory.html#abf5b5d571215f7085fe12c4cd472af8a',1,'NVCategory::create_from_offsets()']]],
+  ['create_5ffrom_5foffsets',['create_from_offsets',['../classNVStrings.html#a982c62faa84db45f5698966e7867fe1d',1,'NVStrings::create_from_offsets()'],['../classNVCategory.html#a998deb14d5ca1050af7d540c6936dde8',1,'NVCategory::create_from_offsets()']]],
   ['create_5ffrom_5fstrings',['create_from_strings',['../classNVStrings.html#a0fa1e986f96cce813091c83bc1b437d3',1,'NVStrings::create_from_strings()'],['../classNVCategory.html#a506f8a1bd8b935383a9e3631162d2f0e',1,'NVCategory::create_from_strings(NVStrings &amp;strs)'],['../classNVCategory.html#a257f36e17c47fa84a301503524e0d061',1,'NVCategory::create_from_strings(std::vector&lt; NVStrings *&gt; &amp;strs)']]],
   ['create_5findex',['create_index',['../classNVStrings.html#ad3f4f0afb3ce81b689af581610c52e9f',1,'NVStrings::create_index()'],['../classNVCategory.html#a3889757a26075d608c28ee6b114e2d51',1,'NVCategory::create_index()']]],
   ['create_5fipc_5ftransfer',['create_ipc_transfer',['../classNVStrings.html#aa1de85578e794a315f189d42b2235033',1,'NVStrings::create_ipc_transfer()'],['../classNVCategory.html#af9a6b95836c76099dd88832c7252905d',1,'NVCategory::create_ipc_transfer()']]],
