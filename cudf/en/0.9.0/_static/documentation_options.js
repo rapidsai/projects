656c60180a4308f9b0bc3c68aa63111a8ec38d27
gpuCI <rapids@ai>
2019-07-01 09:02:55 +0000

    AUTO gpuCI Docs Update

--- cudf/en/0.9.0/_static/documentation_options.js
+++ cudf/en/0.9.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0a+834.gfa94941.dirty',
+    VERSION: '0.9.0a+840.g853a235.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
