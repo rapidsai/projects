5cade94cc1abc34e6374c895811cd03e67035190
gpuCI <38199262+GPUtester@users.noreply.github.com>
2019-09-09 09:05:38 +0000

    [gpuCI] Update docs for v0.10

--- libnvstrings/0.10.0/search/classes_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['nvcategory_161',['NVCategory',['../classNVCategory.html',1,'']]],
-  ['nvstrings_162',['NVStrings',['../classNVStrings.html',1,'']]],
-  ['nvtext_163',['NVText',['../classNVText.html',1,'']]]
-];
