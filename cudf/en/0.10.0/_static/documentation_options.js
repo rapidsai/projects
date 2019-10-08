061324c3fb63a724351a5573db64e3bde83eba61
gpuCI <38199262+GPUtester@users.noreply.github.com>
2019-10-08 08:28:02 +0000

    [gpuCI] Update docs for v0.10

--- cudf/en/0.10.0/_static/documentation_options.js
+++ cudf/en/0.10.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.0a+2343.g6a7d06a50',
+    VERSION: '0.10.0a+2375.g4bc546fd6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
