f07f2896b39ebe07b5ba18ac39ac4716fe0d86f6
gpuCI <38199262+GPUtester@users.noreply.github.com>
2019-10-12 08:25:13 +0000

    [gpuCI] Update docs for v0.10

--- rmm/0.10.0/search/all_a.js
+++ rmm/0.10.0/search/all_a.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['rebind_27',['rebind',['../structrmm_1_1mr_1_1thrust__allocator_1_1rebind.html',1,'rmm::mr::thrust_allocator']]],
-  ['record_28',['record',['../classrmm_1_1Logger.html#a08c5924e48110f8f641a3e9491d1ed87',1,'rmm::Logger']]],
-  ['registerstream_29',['registerStream',['../classrmm_1_1Manager.html#aab9ae171cf3ae4a6002f365cc30729a7',1,'rmm::Manager']]],
-  ['resource_30',['resource',['../classrmm_1_1mr_1_1thrust__allocator.html#aa36bf648f33309f8fadda7e449224970',1,'rmm::mr::thrust_allocator']]],
-  ['rmm_5fallocator_31',['rmm_allocator',['../classrmm__allocator.html',1,'']]],
-  ['rmmoptions_5ft_32',['rmmOptions_t',['../structrmmOptions__t.html',1,'']]]
+  ['setpointer_33',['setPointer',['../classrmm_1_1LogIt.html#a6bee1f58cdd54d2f4651863c8999d4ca',1,'rmm::LogIt']]],
+  ['size_34',['size',['../structcnmemDevice__t__.html#aa2b1f9393452d7548dd1d801a6915546',1,'cnmemDevice_t_']]],
+  ['stream_35',['stream',['../classrmm_1_1mr_1_1thrust__allocator.html#abf19e367462013c948c074cc9b7eed4b',1,'rmm::mr::thrust_allocator']]],
+  ['streams_36',['streams',['../structcnmemDevice__t__.html#a2c6fb71f221ba9d4af844f7cd929caab',1,'cnmemDevice_t_']]],
+  ['streamsizes_37',['streamSizes',['../structcnmemDevice__t__.html#a735d6a6e1406e1048bbb2e956114639b',1,'cnmemDevice_t_']]],
+  ['supports_5fstreams_38',['supports_streams',['../classrmm_1_1mr_1_1cnmem__memory__resource.html#a5be2df0ff53ce6cb9c494845f5664ee9',1,'rmm::mr::cnmem_memory_resource::supports_streams()'],['../classrmm_1_1mr_1_1cuda__memory__resource.html#af3319eeb30d26e14eb8195e6e159d746',1,'rmm::mr::cuda_memory_resource::supports_streams()'],['../classrmm_1_1mr_1_1device__memory__resource.html#a04598a0abd19625d081a69599360b215',1,'rmm::mr::device_memory_resource::supports_streams()'],['../classrmm_1_1mr_1_1managed__memory__resource.html#ad01bf649a47380eef0bff9fb37d94a23',1,'rmm::mr::managed_memory_resource::supports_streams()']]]
 ];
--- rmm/0.10.0/search/enums_0.js
+++ rmm/0.10.0/search/enums_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['pool_5foptions_91',['pool_options',['../classrmm_1_1mr_1_1cnmem__memory__resource.html#abc5e59e9f7fe501ad156f8c531c9c28f',1,'rmm::mr::cnmem_memory_resource']]]
+  ['memory_5fkind_91',['memory_kind',['../classrmm_1_1mr_1_1cnmem__memory__resource.html#a53423dac6d804f073c50ee7cbe2f1b08',1,'rmm::mr::cnmem_memory_resource']]]
 ];
