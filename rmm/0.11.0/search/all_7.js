b1c83dbc18f1b9e1112beef9448cb80914e2e1f4
gpuCI <38199262+GPUtester@users.noreply.github.com>
2019-10-17 20:45:30 +0000

    [gpuCI] Update docs for v0.11

---
+++ rmm/0.11.0/search/all_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['managed_22',['MANAGED',['../classrmm_1_1mr_1_1cnmem__memory__resource.html#abc5e59e9f7fe501ad156f8c531c9c28faf59a25f2594f469f0bfccad7f8f13744',1,'rmm::mr::cnmem_memory_resource']]],
+  ['managed_5fmemory_5fresource_23',['managed_memory_resource',['../classrmm_1_1mr_1_1managed__memory__resource.html',1,'rmm::mr']]],
+  ['manager_24',['Manager',['../classrmm_1_1Manager.html',1,'rmm']]]
+];
---
+++ rmm/0.11.0/search/enumvalues_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cuda_92',['CUDA',['../classrmm_1_1mr_1_1cnmem__memory__resource.html#abc5e59e9f7fe501ad156f8c531c9c28faa33b7755e5f9b504d2d038eaca4ff28d',1,'rmm::mr::cnmem_memory_resource']]]
+];
