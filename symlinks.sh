c6df11bfec2c0ac8312e54d3fbfc5326dddc18bb
AJ Schmidt <aschmidt@nvidia.com>
2020-03-03 14:32:10 -0500

    update symlinks script

--- symlinks.sh
+++ symlinks.sh
@@ -15,10 +15,14 @@ NIGHTLY_FOLDER=$(echo "0.$((${STABLE_VERSION} + 1)).0")
 
 
 for FOLDER in */ ; do
+  # Skip theme folder
+  if [ ${FOLDER} = "theme/" ]; then
+    continue
+  fi
+
   cd ${FOLDER}
   echo ""
   echo "${FOLDER}--------"
-  SYMLINKS_CHANGED=0
   if [ -d "en" ]; then
     cd en
   fi
@@ -28,27 +32,21 @@ for FOLDER in */ ; do
     rm -rf stable latest
     ln -s ${STABLE_FOLDER} stable
     ln -s ${STABLE_FOLDER} latest
-    SYMLINKS_CHANGED=1
     echo "stable & latest point to ${STABLE_FOLDER}"
   fi
 
   if [ -d "${LEGACY_FOLDER}" ]; then
     rm -rf legacy
     ln -s ${LEGACY_FOLDER} legacy
-    SYMLINKS_CHANGED=1
     echo "legacy points to ${LEGACY_FOLDER}"
   fi
 
   if [ -d "${NIGHTLY_FOLDER}" ]; then
     rm -rf nightly
     ln -s ${NIGHTLY_FOLDER} nightly
-    SYMLINKS_CHANGED=1
     echo "nightly points to ${NIGHTLY_FOLDER}"
   fi
 
-  if [ $SYMLINKS_CHANGED = 0 ]; then
-  echo "No symlinks added"
-  fi
   echo "---------------"
   echo ""
   cd ${PROJ_ROOT}
