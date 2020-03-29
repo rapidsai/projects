79dc4a3b072d5de9675f918204f4634f5a203c98
gpuCI <38199262+GPUtester@users.noreply.github.com>
2020-03-29 08:20:26 +0000

    [gpuCI] Update docs for v0.13

--- cudf/en/0.13.0/_static/documentation_options.js
+++ cudf/en/0.13.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.13.0a+4800.gb8c492489',
+    VERSION: '0.12.0+4220.gab0f51b68',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
